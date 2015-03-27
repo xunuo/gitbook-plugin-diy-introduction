4b70e0c9b78286c67ca76b09da3d96fe215bf996
nuo.xun <nuo.xun@alibaba-inc.com>
2015-03-27 14:56:02 +0800

    ci

--- book/plugin.js
+++ book/plugin.js
@@ -1,12 +1,20 @@
-
 require(["gitbook"], function(gitbook) {
+    
+    var pluginConfig;
+
+    var changeText = function(){
+    	if(!pluginConfig || !pluginConfig['introduction-text'])return;
+		var introText = pluginConfig['introduction-text'] ? pluginConfig['introduction-text'] : 'Introduction';
+        $('.summary li[data-level="0"] a').html(introText);
+    };
 
     gitbook.events.bind("start", function(e, config) {
-        alert(JSON.stringify(config));
+    	pluginConfig = config;
+    	changeText();
     });
 
     gitbook.events.bind("page.change", function() {
-
+    	changeText();
     });
 
     gitbook.events.bind("exercise.submit", function(e, data) {
